dee8995805e7d37ee75345e5dc2f176c42ee2abc
Manh Pham Van (Opn VN - Dev Manager) <manhpham90vn@icloud.com>
2024-08-26 09:07:23 +0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -29,9 +29,18 @@ echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zsh
 echo 'eval "$(pyenv init -)"' >> ~/.zshrc
 source ~/.zshrc
 sudo apt update
-sudo apt install -y build-essential libssl-dev zlib1g-dev \
-    libbz2-dev libreadline-dev libsqlite3-dev curl git \
-    libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
-
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-wget https://downloads.1password.com/linux/debian/amd64/stable/1password-latest.deb
+sudo apt install -y build-essential \
+    libssl-dev \
+    zlib1g-dev \
+    libbz2-dev \
+    libreadline-dev \
+    libsqlite3-dev \
+    curl \
+    git \
+    libncursesw5-dev \
+    xz-utils \
+    tk-dev \
+    libxml2-dev \
+    libxmlsec1-dev \
+    libffi-dev \
+    liblzma-dev
